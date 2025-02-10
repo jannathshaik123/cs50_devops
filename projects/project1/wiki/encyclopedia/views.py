37af893f2391e1dd22796d79435ec9e0c4056240
jannathshaik123 <jannathshaik4@gmail.com>
2025-02-11 00:50:40 +0400

    started working on the wiki project: Tried to work on creating the edit page

--- projects/project1/wiki/encyclopedia/views.py
+++ projects/project1/wiki/encyclopedia/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from markdown2 import markdown
 from . import util
+from django import forms
+
+class NewPageForm(forms.Form): 
+    content = forms.CharField(widget=forms.Textarea, label="Content")
 
 def index(request):
     return render(request, "encyclopedia\index.html", {
@@ -9,10 +13,20 @@ def index(request):
 
 def entry(request,title):
     content = util.get_entry(title)
+    if content == None:
+        content = "## Page was not found"
     content = markdown(content)
     return render(request, "encyclopedia\entry.html", {
         "content": content,
     })
 
-def edit(request):
-    return render(request, "encyclopedia\edit.html")
+def edit(request,title):
+    if request.method == "POST":
+        content = request.POST.get("content")
+        util.save_entry(title,content)
+        return render(request, "encyclopedia\edit.html", {
+            "content": markdown(content),
+        })
+    content = util.get_entry(title)
+    return render(request, "encyclopedia\edit.html", {
+        "content": markdown(content)})
