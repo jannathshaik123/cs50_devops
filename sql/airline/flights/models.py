49bb34e341a460902d14e59b883c2940bb871a50
jannathshaik123 <jannathshaik4@gmail.com>
2025-03-12 14:41:41 +0400

    Added passenger details, and allowed passengers to add flights

--- sql/airline/flights/models.py
+++ sql/airline/flights/models.py
@@ -6,7 +6,7 @@ class Airport(models.Model):
     city = models.CharField(max_length=64)
     
     def __str__ (self):
-        return f"{self.city}: {self.code}"
+        return f"{self.city}({self.code})"
     
     
 class Flight(models.Model):
@@ -16,3 +16,11 @@ class Flight(models.Model):
     
     def __str__ (self):
         return f"{self.id}: {self.origin} to {self.destination}"
+
+class Passenger(models.Model):
+    first = models.CharField(max_length=64)
+    last = models.CharField(max_length=64)
+    flights = models.ManyToManyField(Flight, blank=True, related_name="passengers")
+    
+    def __str__ (self):
+        return f"{self.first} {self.last}"
