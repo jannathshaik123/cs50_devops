6a65f4fe0cb5aadc2e027c40dd9fb86385a90bb4
jannathshaik123 <jannathshaik4@gmail.com>
2025-03-17 13:34:16 +0400

    created the index page layout with auctions listing

--- projects/project2/commerce/auctions/admin.py
+++ projects/project2/commerce/auctions/admin.py
@@ -1,3 +1,11 @@
-from django.contrib import admin
+# Register models to be displayed in the Django admin interfaces
+import django.contrib.admin
+from .models import User, AuctionListing, Bid, Comment, Watchlist, Category
 
 # Register your models here.
+django.contrib.admin.site.register(User)
+django.contrib.admin.site.register(AuctionListing)
+django.contrib.admin.site.register(Bid)
+django.contrib.admin.site.register(Comment)
+django.contrib.admin.site.register(Watchlist)
+django.contrib.admin.site.register(Category)
--- projects/project2/commerce/auctions/models.py
+++ projects/project2/commerce/auctions/models.py
@@ -3,4 +3,66 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    # it inherits from the the AbstractUser (so it already have the username password and email!)
+    image = models.URLField(default="https://image.ibb.co/jw55Ex/def_face.jpg")
+
+    def __str__(self):
+        return self.username
+
+class AuctionListing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    starting_bid = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.URLField(blank=True)
+    category = models.CharField(max_length=64, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    max_bidder = models.ForeignKey(User, on_delete=models.CASCADE, related_name="possible_winner", blank=True, null=True)
+    max_bid = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="creator")
+    ## winner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="won_listings", blank=True, null=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+    
+    def max_bid_amount(self):
+        if self.max_bid == 0:
+            return self.starting_bid
+        return self.max_bid
+    
+    def winner(self):
+        if self.active:
+            return None
+        return self.max_bidder
+
+class Bid(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE, related_name="auctioner")
+    item_bid_on = models.ForeignKey(AuctionListing, on_delete=models.CASCADE, related_name="bids")
+    
+
+    def __str__(self):
+        return f"{self.bidder} bid {self.amount} on {self.item_bid_on.title}"
+    
+class Comment(models.Model):
+    text = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commentor")
+    item = models.ForeignKey(AuctionListing, on_delete=models.CASCADE, related_name="itemcomments")
+    timepost = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} commented {self.texted} on {self.listing}"
+    
+class Watchlist(models.Model):
+    watcher = models.ForeignKey(User, on_delete=models.CASCADE, related_name="active_watchlist")
+    listing = models.ForeignKey(AuctionListing, on_delete=models.CASCADE, related_name="watchlist")
+
+    def __str__(self):
+        return f"{self.user} - {self.listing}"
+    
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+    listings = models.ManyToManyField(AuctionListing, blank=True, related_name="categories")
+
+    def __str__(self):
+        return self.name
