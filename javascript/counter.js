3eaba29e04c9be31a580360ad7f364eb67e5f44d
jannathshaik123 <jannathshaik4@gmail.com>
2025-03-29 00:40:03 +0400

    Finished basics of Javascript Lesson6

--- javascript/counter.js
+++ javascript/counter.js
@@ -1,13 +1,15 @@
-let count = 0;
+if (!localStorage.getItem("count")) {
+  localStorage.setItem("count", 0);
+}
+
 function counter() {
+  let count = localStorage.getItem("count");
   count++;
   document.querySelector("h1").innerHTML = count;
-
-  if (count % 10 === 0) {
-    alert(`Counter is ${count}`);
-  }
+  localStorage.setItem("count", count);
 }
 document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("h1").innerHTML = localStorage.getItem("count");
   // document.querySelector("button").addEventListener('onclick',counter);
   document.querySelector("button").onclick = counter;
 });
